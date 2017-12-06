87f3b2f49df5dc5e378d25a1f6b525da3c801984
rossihwang <rossihwang@gmail.com>
2017-12-06 13:02:10 +0800

    Add chapter 5

---
+++ chapter5/forrange.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    double prices[5] = {4.99, 10.99, 6.87, 7.99, 8.49};
+    for (double x: prices) {
+        cout << x << endl;
+    }
+    return 0;
+}
